f90bc544d518c9c800596a6824ec3fde08bbc389
Ryan Doyle <39569215+ryandoyle5401@users.noreply.github.com>
2019-01-18 17:39:00 -0500

    Added OI

--- src/main/java/frc/robot/OI.java
+++ src/main/java/frc/robot/OI.java
@@ -7,6 +7,10 @@
 
 package frc.robot;
 
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.buttons.Button;
+import edu.wpi.first.wpilibj.buttons.JoystickButton;
+
 /**
  * This class is the glue that binds the controls on the physical operator
  * interface to the commands and command groups that allow control of the robot.
@@ -24,7 +28,7 @@ public class OI {
   // by subclassing Button you can create custom triggers and bind those to
   // commands the same as any other Button.
 
-  //// TRIGGERING COMMANDS WITH BUTTONS
+  //// TRIGGERING COMMANDS WITH BUTTONS0
   // Once you have a button, it's trivial to bind it to a button in one of
   // three ways:
 
@@ -39,5 +43,58 @@ public class OI {
   // Start the command when the button is released and let it run the command
   // until it is finished as determined by it's isFinished method.
   // button.whenReleased(new ExampleCommand());
+
+  /*** Objects ***/
+    //Joysticks
+    //Need to be public since they are called from other classes. 
+  public Joystick xboxDriver = new Joystick(RobotMap.XBOX_CONTROLLER_DRIVER);
+  public Joystick xboxOperator = new Joystick(RobotMap.XBOX_CONTROLLER_OPERATOR);
+
+    //Buttons (Driver)
+  Button xboxA_Driver			  = new JoystickButton(xboxDriver, 1);
+	Button xboxB_Driver			  = new JoystickButton(xboxDriver, 2);
+	Button xboxX_Driver			  = new JoystickButton(xboxDriver, 3);
+	Button xboxY_Driver			  = new JoystickButton(xboxDriver, 4);
+	Button xboxLeftBumper_Driver  = new JoystickButton(xboxDriver, 5);
+	Button xboxRightBumper_Driver = new JoystickButton(xboxDriver, 6);
+	Button xboxBack_Driver		  = new JoystickButton(xboxDriver, 7);
+	Button xboxStart_Driver		  = new JoystickButton(xboxDriver, 8);
+	Button xboxL3_Driver		  = new JoystickButton(xboxDriver, 9);
+	Button xboxR3_Driver		  = new JoystickButton(xboxDriver, 10);
   
+    //Buttons (Operator)
+  Button xboxA_Operator			= new JoystickButton(xboxOperator, 1);
+	Button xboxB_Operator			= new JoystickButton(xboxOperator, 2);
+	Button xboxX_Operator			= new JoystickButton(xboxOperator, 3);
+	Button xboxY_Operator			= new JoystickButton(xboxOperator, 4);
+	Button xboxLeftBumper_Operator  = new JoystickButton(xboxOperator, 5);
+	Button xboxRightBumper_Operator = new JoystickButton(xboxOperator, 6);
+	Button xboxBack_Operator		= new JoystickButton(xboxOperator, 7);
+	Button xboxStart_Operator		= new JoystickButton(xboxOperator, 8);
+	Button xboxL3_Operator		  	= new JoystickButton(xboxOperator, 9);
+	Button xboxR3_Operator		  	= new JoystickButton(xboxOperator, 10);
+
+  public OI(){
+  }
+
+  public double xboxAxis(Joystick xboxController, int xboxAxis){
+    return xboxController.getRawAxis(xboxAxis);
+  }
+
+  public boolean xboxButton(Joystick xboxController, int xboxButton){
+    return xboxController.getRawButton(xboxButton);
+  }
+
+  public int xboxAxisAsButton(Joystick xboxController, int xboxAxis){
+    if(xboxController.getRawAxis(xboxAxis) > RobotMap.AXIS_THRESHOLD){
+      return 1;
+    }
+    else if(xboxController.getRawAxis(xboxAxis) < (-1 * RobotMap.AXIS_THRESHOLD)){
+      return -1;
+    }
+    else{
+      return 0;
+    }
+  }
 }
+
